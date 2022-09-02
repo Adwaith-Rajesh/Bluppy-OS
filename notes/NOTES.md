d20a3065e7dacf7e17c93238e716df62980707e0
Adwaith Rajesh <adwaithrajesh3180@gmail.com>
2022-09-02 21:24:26 +0530

    Add the FAT headers to bootloader

---
+++ notes/NOTES.md
@@ -0,0 +1,5 @@
+## LBA to CHS
+
+## FAT file system
+
+- https://wiki.osdev.org/FAT
--- src/bootloader/boot.asm
+++ src/bootloader/boot.asm
@@ -4,6 +4,43 @@ bits 16         ; use 16 bit compilation mode
 
 %define ENDL 0x0D, 0x0A
 
+;
+; FAT 12 headers
+;
+
+
+jmp short start
+nop
+
+
+bpb_oem_identifier:             db 'MSWIN4.1'       ; 8 bits
+bpb_bytes_per_sector:           dw 512
+bpb_sectos_per_cluster:         db 1
+bpb_reserved_sectors:           dw 2
+bpb_fat_count:                  db 2
+bpb_dir_entries_count:          dw 0E0h
+bpb_sector_count:               dw 2880             ; 2880 * 512 = 1.44 MB
+bpb_media_descriptor_type:      db 0F0h             ; 3.5' Floppy disk
+bpb_sectors_per_fat:            dw 9                ; 9 sectors per FAT
+bpb_sectors_per_track:          dw 18
+bpb_heads:                      dw 2
+bpb_hidden_sectors:             dd 0
+bpb_large_sector_count:         dd 0
+
+
+;
+; Extended boot record
+;
+
+ebr_drive_number:               db 0                    ; 0x00 for a floppy disk and 0x80 for hard disks
+                                db 0                    ; reserve bit
+ebr_signature:                  db 29h
+ebr_volume_id:                  db 12h, 34h, 56h, 78h   ; 4 bytes serial number, can be anything
+ebr_voulme_label:               db 'BLUPPY   OS'        ; 11 bytes padded with spaces
+ebr_system_id:                  db 'FAT12   '           ; 8 bytes padded with space
+
+
+
 start:
     jmp main
 
@@ -25,7 +62,7 @@ main:
     hlt
 
 ;
-; prints a string to the string
+; prints a string to the terminal
 ; params:
 ;   - ds:si -> pointer to the string
 ;
